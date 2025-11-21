624e0faa0bddd743a29f29d269c59cd77b929033
Naimul Islam <ms0413354@gmail.com>
2025-11-22 02:03:30 +0600

    complete login page design

--- src/modules/login/ui/views/loging-views.tsx
+++ src/modules/login/ui/views/loging-views.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import {
 	InputGroup,
@@ -5,9 +6,42 @@ import {
 	InputGroupInput,
 } from '@/components/ui/input-group';
 import { Label } from '@/components/ui/label';
-import { MailIcon, UsersIcon } from 'lucide-react';
+import {
+	EyeClosedIcon,
+	EyeIcon,
+	LockIcon,
+	MailIcon,
+	UsersIcon,
+} from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { loginSchema } from '../../schema/login-schema';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+interface Props {
+	email: string;
+	password: string;
+}
 
 export const LoginViews = () => {
+	const [passwordType, setPasswordType] = useState(true);
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+		resolver: yupResolver(loginSchema),
+	});
+
+	const onSubmit = (data: Props) => {
+		console.log(data);
+	};
 	return (
 		<div className="container mx-auto min-h-screen flex items-center justify-center">
 			<Card className="rounded w-2xl">
@@ -20,18 +54,55 @@ export const LoginViews = () => {
 				</CardHeader>
 
 				<CardContent>
-					<div>
-						<Label className="font-sans mb-2">Email</Label>
-						<InputGroup className="">
-							<InputGroupInput
-								className="font-sans"
-								placeholder="you@example.com"
-							/>
-							<InputGroupAddon>
-								<MailIcon />
-							</InputGroupAddon>
-						</InputGroup>
-					</div>
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div>
+							<Label className="font-sans mb-2">Email</Label>
+							<InputGroup className="">
+								<InputGroupInput
+									type="email"
+									{...register('email')}
+									className="font-sans"
+									placeholder="you@example.com"
+								/>
+								<InputGroupAddon>
+									<MailIcon />
+								</InputGroupAddon>
+							</InputGroup>
+
+							{errors.email && (
+								<p className="text-red-500 mt-1">{errors.email.message}</p>
+							)}
+						</div>
+
+						<div className="mt-5">
+							<Label className="font-sans mb-2">Password</Label>
+							<InputGroup className="">
+								<InputGroupInput
+									type={passwordType ? 'password' : 'text'}
+									{...register('password')}
+									className="font-sans"
+									placeholder="****************"
+								/>
+								<InputGroupAddon align="inline-start">
+									<LockIcon />
+								</InputGroupAddon>
+								<InputGroupAddon
+									align="inline-end"
+									onClick={() => setPasswordType(!passwordType)}
+								>
+									{passwordType ? <EyeIcon /> : <EyeClosedIcon />}
+								</InputGroupAddon>
+							</InputGroup>
+
+							{errors.password && (
+								<p className="text-red-500 mt-1">{errors.password.message}</p>
+							)}
+						</div>
+
+						<Button type="submit" className="w-full rounded mt-5">
+							Login
+						</Button>
+					</form>
 				</CardContent>
 			</Card>
 		</div>
