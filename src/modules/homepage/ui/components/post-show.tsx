3ed7c066b5efaebf2e65bc6c0b465150f9bef216
Naimul Islam <ms0413354@gmail.com>
2025-11-23 13:10:36 +0600

    complete project

--- src/modules/homepage/ui/components/post-show.tsx
+++ src/modules/homepage/ui/components/post-show.tsx
@@ -12,12 +12,17 @@ import {
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import {
+	Bell,
+	Bookmark,
 	EllipsisVerticalIcon,
 	Heart,
 	MessageCircle,
 	Mic,
 	SendHorizonalIcon,
 	Share,
+	SquarePen,
+	Trash,
+	X,
 } from 'lucide-react';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import dayjs from 'dayjs';
@@ -41,6 +46,29 @@ export const PostShow = () => {
 	const { mutate, data, isPending, error: likeError } = useLikeAndUnlike();
 	const { mutate: commentMutate } = useAddComment();
 	const [comment, setComment] = useState('');
+
+	const postOptions = [
+		{
+			icon: Bookmark,
+			text: 'Save Post',
+		},
+		{
+			icon: Bell,
+			text: 'Turn On Notification',
+		},
+		{
+			icon: X,
+			text: 'Hide',
+		},
+		{
+			icon: SquarePen,
+			text: 'Edit Post',
+		},
+		{
+			icon: Trash,
+			text: 'Delete Post',
+		},
+	];
 
 	const handleLove = (id: string) => {
 		mutate(
@@ -107,7 +135,14 @@ export const PostShow = () => {
 								</DropdownMenuTrigger>
 
 								<DropdownMenuContent className="rounded">
-									<DropdownMenuItem>dhasvhgsnab</DropdownMenuItem>
+									{postOptions.map((option, i) => (
+										<DropdownMenuItem key={i} className="mt-3 gap-5">
+											<option.icon className="text-blue-500 size-5" />
+											<span className="text-[16px] font-sans hover:text-blue-500">
+												{option.text}
+											</span>
+										</DropdownMenuItem>
+									))}
 								</DropdownMenuContent>
 							</DropdownMenu>
 						</div>
@@ -190,6 +225,38 @@ export const PostShow = () => {
 									<SendHorizonalIcon className="size-5" />
 								</InputGroupAddon>
 							</InputGroup>
+						</div>
+
+						<div className="w-full">
+							{post.comment.length > 3 && (
+								<p className="mt-3 font-sans text-[16px] text-gray-500">
+									View {post.comment.length - 3} previous comments
+								</p>
+							)}
+
+							{post?.comment
+								?.slice()
+								.reverse()
+								.slice(0, 3)
+								.map((c, i) => (
+									<div key={i}>
+										<div className="flex items-center gap-3 mt-3">
+											<div>
+												<Avatar>
+													<AvatarImage src="/comment_img.png" />
+												</Avatar>
+											</div>
+											<div className="bg-secondary rounded w-full py-3 px-5">
+												<h5 className="font-sans text-[16px] font-semibold hover:underline">
+													{c.user.firstName} {c.user.lastName}
+												</h5>
+												<p className="font-sans text-sm text-gray-500">
+													{c.comment}
+												</p>
+											</div>
+										</div>
+									</div>
+								))}
 						</div>
 					</CardFooter>
 				</Card>
